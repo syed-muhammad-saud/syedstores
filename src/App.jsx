646b69da52363a2a8761329b30154c777be5db49
syed-muhammad-saud <saudshah.03p4@gmail.com>
2024-10-28 22:29:53 +0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,35 @@ import './App.css';
 import React from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/main/NavBar.jsx';
-import ScrollToTop from './components/ScrollToTop.jsx';
-import Products from './components/products/Products.jsx';
-import ProductsDetail from './components/products/ProductsDetail.jsx';
-import SearchResult from './components/products/SearchResult.jsx';
-import Blogs from './components/Blogs.jsx';
-import Contact from './components/Contact.jsx';
-import NotFound from './components/notFound/NotFound.jsx';
-import Footer from './components/main/Footer.jsx';
-import Checkout from './components/loginsignup/Checkout.jsx';
-import Auth from './components/loginsignup/Auth.jsx';
-import Cart from '../src/components/cart/Cart.jsx'; 
+import Navbar from './components/main/NavBar';
+import ScrollToTop from './components/ScrollToTop'; 
+// import Home from './components/Home'; 
+import Products from './components/products/Products';
+import ProductDetails from './components/products/productDetails'; 
+import SearchResult from './components/products/SearchResult'
+import Blogs from './components/Blogs'; 
+import Contact from './components/Contact'; 
+// import Login from './components/loginsignup/Auth';
+// import Signup from './components/loginsignup/signup';
+import NotFound  from './components/notFound/NotFound';
+import Footer from './components/main/Footer';
+import Cart from './components/loginsignup/cart.jsx';
+import Checkout from './components/loginsignup/Checkout';
+import Auth from './components/loginsignup/Auth';
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <Navbar/>
       <ScrollToTop />
       <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/" element={<Products />} />
-        <Route path="/products/:id" element={<ProductsDetail />} />
-        <Route path="/search" element={<SearchResult />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="/search" element={<SearchResult />} /> 
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/cart" element={<Cart />} /> 
+        <Route path="/cart" element={<Cart />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="*" element={<NotFound />} />
