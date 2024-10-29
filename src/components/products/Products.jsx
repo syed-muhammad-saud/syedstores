9fa4d0b09aa8312d1a6790210816dd36a32110db
syed-muhammad-saud <saudshah.03p4@gmail.com>
2024-10-29 11:01:47 +0500

    Loader

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './Products.css';
 import { Link } from 'react-router-dom';
-import Loader from '../loader/Loader.jsx';
+import Loader from '../loader/Loader';
 
 const Products = () => {
   const [products, setProducts] = useState([]);
